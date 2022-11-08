d9f056bdae12a50c86fce7b5e27e44b437741741
Nick Everitt <nicholaseveritt@gmail.com>
2022-11-08 21:28:45 +0000

    Refactor board representation

--- chess2.hs
+++ chess2.hs
@@ -1,13 +1,49 @@
-data Piece = Pawn | Knight | Bishop | Rook | Queen | King deriving (Show, Eq)
+import Data.List
+
+data PieceKind = Pawn | Knight | Bishop | Rook | Queen | King deriving (Show, Eq)
 
 data Player = White | Black deriving (Show, Eq)
 
-type Square = Maybe (Piece, Player)
+type Position = (Int, Int)
 
-type Board = [Square]
+type Piece = (PieceKind, Player, Position)
 
-data CastleSide = Kingside | Queenside deriving (Show, Eq)
+type Board = [Piece]
 
-type Castleables = [CastleSide]
+isLegal :: Position -> Bool
+isLegal (x, y)
+  | x < 0 = False
+  | x > 7 = False
+  | y < 0 = False
+  | y > 7 = False
+  | otherwise = True
+
+isAt :: Position -> Piece -> Bool
+isAt posn (pi, pl, po) = po == posn 
+
+reposition :: Piece -> Position -> Piece
+reposition (piece, player, posn) newPosn = (piece, player, newPosn)
+
+occupant :: Board -> Position -> Maybe Piece
+occupant b p = find (isAt p) b
+
+remove :: Board -> Position -> Board
+remove board posn = filter (\piece -> not (isAt posn piece)) board
+
+place :: Board -> Piece -> Board
+place board piece = piece : board
+
+
+move :: Board -> Position -> Position -> Board
+move board oldPosn newPosn =
+  case occupant board oldPosn of
+    Just piece -> (reposition piece newPosn) : (remove board oldPosn) 
+    Nothing -> board
+
+
+
+board1 :: Board
+board1 = [(King, White, (3, 0)), (Queen, Black, (4,1))]
+
 
 
